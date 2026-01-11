908e59daddd20e0069c230d274364f85d6d9274d
kyle hansen <kylethansen17@gmail.com>
2026-01-11 22:40:19 +0900

    added enum status to the application model and created note model and associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,13 @@
 class Application < ApplicationRecord
   belongs_to :user
+  has_many :notes, dependent: :destroy
+
+  enum status: {
+    applied: 0,
+    interview: 1,
+    offer: 2,
+    rejected: 3
+  }
+
+  validates :company_name, :job_title, :status, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_10_095256) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_11_133741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,14 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_10_095256) do
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.bigint "application_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["application_id"], name: "index_notes_on_application_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +47,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_10_095256) do
   end
 
   add_foreign_key "applications", "users"
+  add_foreign_key "notes", "applications"
 end
